56243226ae06be8018eb4632ef8f6bd244ce3c0f
BiradarNitesh <121621316+Biradanitesh@users.noreply.github.com>
2026-03-11 12:56:39 +0530

    Update ecommerce_project.sql

--- ecommerce_project.sql
+++ ecommerce_project.sql
@@ -16,8 +16,6 @@ product_name varchar(50),
 category varchar(50),
 price decimal(10,2)
 );
-alter table products 
-modify column price decimal(10,2);
 
 create table orders
 (
@@ -42,8 +40,6 @@ Insert into customers values
 (2,'Priya','Delhi','2023-02-15'),
 (3,'Amit','Bangalore','2023-03-20'),
 (4,'Sneha','Hyderabad','2023-04-05');
-
-INSERT INTO customers VALUES
 (5,'Arjun','Pune','2023-05-12'),
 (6,'Meera','Chennai','2023-06-18'),
 (7,'Ravi','Kolkata','2023-07-20'),
@@ -54,7 +50,6 @@ Insert into products values
 (102,'Headphones','Electronics',2000),
 (103,'Shoes','Fashion',3000),
 (104,'Watch','Accessories',5000);
-INSERT INTO products VALUES
 (105,'Smartphone','Electronics',40000),
 (106,'Backpack','Fashion',1500),
 (107,'Sunglasses','Accessories',2500),
@@ -66,7 +61,6 @@ INSERT INTO orders VALUES
 (1002,2,'2024-01-11'),
 (1003,1,'2024-02-01'),
 (1004,3,'2024-02-05');
-INSERT INTO orders VALUES
 (1005,4,'2024-02-10'),
 (1006,5,'2024-02-12'),
 (1007,6,'2024-02-15'),
@@ -80,7 +74,6 @@ INSERT INTO order_details VALUES
 (3,1002,103,1),
 (4,1003,104,1),
 (5,1004,101,1);
-INSERT INTO order_details VALUES
 (6,1005,105,1),
 (7,1006,106,2),
 (8,1007,108,1),
@@ -88,18 +81,18 @@ INSERT INTO order_details VALUES
 (10,1009,105,1);
 
 
-#Total Revenue
+#Total Revenue?
 select SUM(products.price * order_details.quantity) AS Total_revenue from products
 join order_details on products.product_id=order_details.product_id;
 
-#Revenue by Product
+#Revenue by Product?
 
 select products.product_name,sum(products.price * order_details.quantity) AS Total_revenue from products
 join order_details on products.product_id=order_details.product_id
 group by products.product_name
 order by Total_revenue;
 
-#Top Customers by Spending
+#Top Customers by Spending?
 
 select customer_name,sum(p.price * od.quantity) AS revenue from customers as c
 join orders as o `ecommerce_dataset`
@@ -111,13 +104,13 @@ on od.product_id=p.product_id
 group by customer_name
 order by revenue;
 
-#Sales by Category
+#Sales by Category?
 
 select products.category,sum(products.price * order_details.quantity) AS Total_revenue from products
 join order_details on products.product_id=order_details.product_id
 group by category;
 
-#Monthly Sales Trend
+#Monthly Sales Trend?
 
 select date_format(o.order_date,'%Y-%m') AS month ,sum(p.price * od.quantity) AS Total_revenue from orders as o
 join order_details as od
@@ -127,7 +120,7 @@ on p.product_id=od.product_id
 group by month
 order by month;
 
-#Average Order Value
+#Average Order Value?
 
 select avg(order_total) as avg_order_value from (
 select o.order_id,sum(p.price * od.quantity) AS order_total from orders o
@@ -135,7 +128,7 @@ join order_details od on o.order_id=od.order_id
 join products p on p.product_id = od.product_id
 group BY order_id)t;
 
-# Most Popular Product
+# Most Popular Product?
 
 select product_name,sum(od.quantity) as total_quantity from products as p
 join order_details od on od.product_id=p.product_id
@@ -143,7 +136,7 @@ group by product_name
 order by total_quantity desc
 LIMIT 1;
 
-#Customer Order Frequency
+#Customer Order Frequency?
 
 select customer_name,count(o.order_id) as total_orders from customers c
 left join orders o on c.customer_id=o.customer_id
